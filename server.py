515eb68e74517bdcac8e3f121e8dd5664985cc67
Ly Nguyen <lynguyen237@gmail.com>
2020-10-13 01:20:36 +0000

    Create a draft homepage with Flask

--- server.py
+++ server.py
@@ -4,5 +4,9 @@ from model import connect_to_db
 
 app = Flask(__name__)
 
-if __name__ == '__main__'
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)

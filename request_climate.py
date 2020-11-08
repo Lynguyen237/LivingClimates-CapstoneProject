bcba56f47e87ecda3b07862745bddbe4feced596
Ly Nguyen <lynguyen237@gmail.com>
2020-11-08 00:12:21 +0000

    Update request_climate and remove homepage3.jsx

--- request_climate.py
+++ request_climate.py
@@ -3,11 +3,16 @@ import requests
 
 url = "https://api.meteostat.net/v2/point/climate"
 
+
 def get_climate(lat, lon):
     params = dict(key="key", lat=lat, lon=lon)
     req = requests.get(url, params=params)
     res = req.json() # res is a python dictionary
+    # res['latlon'] = {'lat': lat, 'lon':lon}
     return res
+
+
+
 
 # print (get_climate(40.712776,-74.005974)) # New York
 # params = dict(key="key", lat=40.712776, lon=-74.005974) # New York

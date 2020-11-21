189909a4b3339952668e30f9f63cbcc6dd4b9c8a
Ly Nguyen <lynguyen237@gmail.com>
2020-11-21 19:50:52 +0000

    Show Map using index.js on all pages

--- static/js/index.js
+++ static/js/index.js
@@ -7,10 +7,10 @@ function initMap() {
   // Multiple locations
   const locations = [
     ['Los Angeles', 34.052235, -118.243683],
-    ['Santa Monica', 34.024212, -118.496475],
-    ['Redondo Beach', 33.849182, -118.388405],
-    ['Newport Beach', 33.628342, -117.927933],
-    ['Long Beach', 33.770050, -118.193739]
+    // ['Santa Monica', 34.024212, -118.496475],
+    // ['Redondo Beach', 33.849182, -118.388405],
+    // ['Newport Beach', 33.628342, -117.927933],
+    // ['Long Beach', 33.770050, -118.193739]
   ];
   
   // const locations = [
--- static/js/maps.jsx
+++ static/js/maps.jsx
@@ -1,62 +1,52 @@
 "use strict";
 
-
-// ===========================
-// function MapComponent() {
-//     const [text, updateText] = React.useState('result');
-    
-//     return (
-//         <React.Fragment>
-//             <p>{text}</p>
-//             <button type="button" value="abc" onClick={evt => updateText(evt.target.value)}>Change Text</button>
-//             <button type="button" onClick={() => updateText("anything")}>Change Text</button>
-//         </React.Fragment>
-//     )
-// }
-
-// ============================
-
 function MapComponent(props) {
   console.log('rendering the map')
-  // const options = props.options;
-  // const ref = React.useRef();
   
-  // React.useEffect(() => {
-  //   const createMap = () => props.setMap(new window.google.maps.Map(ref.current, options));
-  //   if (!window.google) { // Create an html element with a script tag in the DOM
-  //     const script = document.createElement('script');
-  //     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBtYZMS7aWpKxyZ20XLWWNEMKb3eo6iOkY&libraries=places';
-  //     document.head.append(script);
-  //     script.addEventListener('load', createMap);
-  //     console.log('and now there is a map')
-  //     return () => script.removeEventListener('load', createMap);
-  //   } else { // Initialize the map if a script element with google url IS found
-  //     createMap();
-  //     console.log('and now there is a map');
-  //   }
-  // }, [options.center.lat]); //Need the value of the lat of options because it does not change
+  // const center = { lat: 34.052235, lng: -118.243683 };
   
-  // if (props.map) {
-  //   console.log('and the map exists')
-  // } else { console.log('but there is no map')}
+  // // Multiple locations
+  // const locations = [
+  //   ['Los Angeles', 34.052235, -118.243683],
+  // ];
+
+  // // The map, centered at Uluru
+  // const map = new google.maps.Map(document.getElementById("map"), {
+  //   zoom: 9,
+  //   center: center,
+  // });
+
+  // // infowindow variable as a new Google Maps Info Window to display a marker's information
+  // let infowindow = new google.maps.InfoWindow({});
+
+
+  // let marker, count;
+
+  // for (count=0; count < locations.length; count++) {
+  //   marker = new google.maps.Marker({
+  //     position: new google.maps.LatLng(locations[count][1],locations[count][2]),
+  //     map: map,
+  //     title: locations[count][0]
+  //   });
+
+  //   google.maps.event.addListener(marker, 'hover', (function (marker, count) {
+  //     return function () {
+  //       infowindow.setContent(locations[count][0]);
+  //       // infowindow.open(map, marker);
+  //     }
+  //   })(marker, count));
+  // }
   
   return (
-    <div id="map"
-      // style={{ height: props.mapDimensions.height, 
-      //   margin: 1,  
-      //   borderRadius: 0.5, 
-      //   width: props.mapDimensions.width }}
-      // ref={ref}
-    >
+    <div id="xyz">
       <h3>My Favorites</h3>
     </div>
   )
 }
 
-// ReactDOM.render(<MapComponent />, document.querySelector('#map'))
 
 
-// ==== List of favorite cities ====
+// ==== Listing favorite cities ====
 function City(props) {
   function saveFavorite(evt) {
    

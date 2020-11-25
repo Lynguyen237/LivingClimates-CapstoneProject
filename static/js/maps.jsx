c371dfd9436f8bc701f43a1dbb2168a5063188b0
Ly Nguyen <lynguyen237@gmail.com>
2020-11-25 04:22:07 +0000

    Get Maps component working in maps.jsx

--- static/js/maps.jsx
+++ static/js/maps.jsx
@@ -2,16 +2,12 @@
 
 // ==== Listing favorite cities ====
 function Favorites() {
-  console.log('Favorites function runs')
   const [favoriteDict, setFavoriteDict] = React.useState([])
   
-  // console.log('Before Favorites useEffect')
   React.useEffect(() => {
-    // console.log('Fetching starts')
     fetch('/favorites.json')
     .then((response) => response.json())
     .then((data) => setFavoriteDict(data.favorites))
-    // console.log('Fetching done')
   },[])
 
   // Create an empty array of favorite cities then loop through the favoriteDict to add each city to the array
@@ -26,113 +22,46 @@ function Favorites() {
                       favoriteDict={favoriteDict}
                       />)
   ))}
-  // console.log(favoriteDict)
-  
-  //==== Insert map =====
-  const mapsdiv = React.useRef(null)
-  console.log('rendering the map')
-
-  React.useEffect(() => {
-    const map = new google.maps.Map(mapsdiv.current, {
-      // zoom: 9,
-      // center: center,
-    });
-
-    // infowindow variable as a new Google Maps Info Window to display a marker's information
-    let infowindow = new google.maps.InfoWindow({});
-    let marker;
-    const bounds = new google.maps.LatLngBounds();
-
-    for (const city of Object.keys(favoriteDict)) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(favoriteDict[city].lat, favoriteDict[city].lon),
-        map: map,
-        title: city
-      });
-      
-      bounds.extend(marker.getPosition());
-      
-      google.maps.event.addListener(marker, 'hover', (function (city) {
-        return function () {
-          infowindow.setContent(city);
-        }
-      })(marker, city));
-    }
-    map.fitBounds(bounds);
-  })
-  // ==== End map insertion ====
-
-  console.log('Favorites function ends')
 
   return (
     <React.Fragment>
-      <h3>My Favorites</h3>
-      <div ref={mapsdiv} id="map"></div>
-      {/* <MapComponent title="My Favorite" favoriteDict={favoriteDict} map="map"></MapComponent> */}
+      <MapComponent title="My Favorites" favoriteDict={favoriteDict} map="map"></MapComponent>
       <br/>
       <div id="favorites">{favoriteData}</div>
     </React.Fragment>
   )
 }
 
-// function MapComponent(props) {
-//   console.log('Map Component runs')
-//   const mapsdiv = React.useRef(null)
+// ==== MAPS COMPONENT ====
+function MapComponent(props) {
 
-//   const center = { lat: 34.052235, lng: -118.243683 };
-  
-//   // Multiple locations
-//   const locations = [
-//     ['Los Angeles', 34.052235, -118.243683],
-//   ];
-//   console.log(props.favoriteDict)
-  
-//   console.log('Map component ends before useEffect')
-//   // mapdiv is defined but not assigned
-//   React.useEffect( () => {
-//     console.log(`useEffect runs`)
-//     const map = new google.maps.Map(mapsdiv.current, {
-//     zoom: 9,
-//     center: center,
-//     });
+  const mapsdiv = React.useRef(null)
 
-//     let marker, count;
+  React.useEffect( () => {
+    const map = new google.maps.Map(mapsdiv.current);
+
+    let marker;
+    const bounds = new google.maps.LatLngBounds();
+
+    if (props.favoriteDict) {
+      for (const city of Object.keys(props.favoriteDict)) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(props.favoriteDict[city].lat,
+                                          props.favoriteDict[city].lon),
+          map: map,
+          title: city
+        });
+        bounds.extend(marker.getPosition());
+      }
+    }
+    map.fitBounds(bounds);
+  }, [props.favoriteDict]);
     
-//     console.log(mapsdiv.current)
-//     console.log(`Inside useEffect: ${props.favoriteDict}`)
-//     for (count=0; count < locations.length; count++) {
-//       marker = new google.maps.Marker({
-//         position: new google.maps.LatLng(locations[count][1],locations[count][2]),
-//         map: map,
-//         title: locations[count][0]
-//       });    
-      
-//     // for (const city of Object.keys(props.favoriteDict)) {
-//     //   marker = new google.maps.Marker({
-//     //     position: new google.maps.LatLng(props.favoriteDict[city].lat,
-//     //                                      props.favoriteDict[city].lon),
-//     //     map: map,
-//     //     title: city
-//     //   });
 
-//       // infowindow variable as a new Google Maps Info Window to display a marker's information
-//       google.maps.event.addListener(marker, 'hover', (function (marker, count) {
-//         return function () {
-//           infowindow.setContent(locations[count][0]);
-//           // infowindow.open(map, marker);
-//         }
-//       })(marker, count));
-//     }
-//     console.log(`useEffect ENDs`)
-//   }, []);
-
-
-//   return (
-//     <React.Fragment>
-//     {console.log(`Map Component Return runs ${props.title}, ${props.map}`)}
-//     <h3>{props.title}</h3>
-//     <div ref={mapsdiv} id={props.map}></div>
-//     {console.log('Map Component Return ends')}
-//     </React.Fragment>
-//   )
-// }
+  return (
+    <React.Fragment>
+    <h3>{props.title}</h3>
+    <div ref={mapsdiv} id={props.map}></div>
+    </React.Fragment>
+  )
+}
